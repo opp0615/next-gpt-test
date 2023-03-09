052c1fc693ceccb019d2a3deeffa846231970d24
cloto.jsy <cloto.jsy@kakaoent.com>
2023-03-09 17:16:07 +0900

    환경설정 재재설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
-	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint', 'prettier'],
-	extends: [
-	  'plugin:@typescript-eslint/recommended',
-	  'prettier/@typescript-eslint',
-	  'plugin:prettier/recommended',
-	],
-	rules: {
-	  // Add any custom rules here
-	},
-  };
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    // Add any custom rules here
+  },
+};
---
+++ .next/server/font-loader-manifest.js
@@ -0,0 +1,10 @@
+self.__FONT_LOADER_MANIFEST={
+  "pages": {
+    "/": [
+      "static/media/2aaf0723e720e8b9-s.p.woff2"
+    ]
+  },
+  "app": {},
+  "appUsingSizeAdjust": false,
+  "pagesUsingSizeAdjust": true
+}
