70487998082c615373b7a4044e41607f4e4db21a
cloto.jsy <cloto.jsy@kakaoent.com>
2023-03-09 17:04:35 +0900

    싹 초기화

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint', 'prettier'],
+	extends: [
+	  'plugin:@typescript-eslint/recommended',
+	  'prettier/@typescript-eslint',
+	  'plugin:prettier/recommended',
+	],
+	rules: {
+	  // Add any custom rules here
+	},
+  };
--- src/test/example.test.ts
+++
@@ -1,7 +0,0 @@
-import add from './example';
-
-describe('add', () => {
-  it('adds two numbers', () => {
-    expect(add(2, 3)).toBe(5);
-  });
-});
