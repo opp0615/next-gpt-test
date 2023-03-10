38973a5e46ab5ef09e607acbaa5a9e7b501da27e
cloto.jsy <cloto.jsy@kakaoent.com>
2023-03-10 17:11:56 +0900

    [gpt] react-swipable로 스와이프 구현(동작x)

--- src/EpisodeSection.tsx
+++ src/EpisodeSection.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import EpisodeComponent from './EpisodeComponent';
 import { Episode } from './types';
+import { useSwipeable } from 'react-swipeable';
 
 interface Props {
   title: string;
@@ -8,10 +9,25 @@ interface Props {
 }
 
 const EpisodeSection: React.FC<Props> = ({ title, episodes }) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const handleSwipe = useSwipeable({
+    onSwipedLeft: () => {
+      if (containerRef.current) {
+        containerRef.current.scrollLeft += containerRef.current.clientWidth;
+      }
+    },
+    onSwipedRight: () => {
+      if (containerRef.current) {
+        containerRef.current.scrollLeft -= containerRef.current.clientWidth;
+      }
+    },
+  });
+
   return (
     <div className="flex flex-col">
       <h2 className="mb-4 text-2xl font-bold">{title}</h2>
-      <div className="flex overflow-x-auto">
+      <div className="flex overflow-x-auto" {...handleSwipe} ref={containerRef}>
         {episodes.map((episode) => (
           <div className="flex-none mr-4" key={episode.name}>
             <EpisodeComponent episode={episode} />
