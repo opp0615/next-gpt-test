08cdec62eec744f2456cb22d2fe67f5f9cf13963
cloto.jsy <cloto.jsy@kakaoent.com>
2023-03-10 17:16:25 +0900

    [gpt] 작품 섹션 스크롤을 버튼으로 대체

--- src/EpisodeSection.tsx
+++ src/EpisodeSection.tsx
@@ -1,7 +1,7 @@
 import React, { useRef } from 'react';
 import EpisodeComponent from './EpisodeComponent';
 import { Episode } from './types';
-import { useSwipeable } from 'react-swipeable';
+import { useState } from 'react';
 
 interface Props {
   title: string;
@@ -10,29 +10,55 @@ interface Props {
 
 const EpisodeSection: React.FC<Props> = ({ title, episodes }) => {
   const containerRef = useRef<HTMLDivElement>(null);
+  const [scrollPosition, setScrollPosition] = useState(0);
 
-  const handleSwipe = useSwipeable({
-    onSwipedLeft: () => {
-      if (containerRef.current) {
-        containerRef.current.scrollLeft += containerRef.current.clientWidth;
-      }
-    },
-    onSwipedRight: () => {
-      if (containerRef.current) {
-        containerRef.current.scrollLeft -= containerRef.current.clientWidth;
-      }
-    },
-  });
+  const handleScroll = (amount: number) => {
+    if (containerRef.current) {
+      const newPosition = scrollPosition + amount;
+      setScrollPosition(newPosition);
+      containerRef.current.scrollTo({
+        left: newPosition,
+        behavior: 'smooth',
+      });
+    }
+  };
+
+  const scrollLeft = () => handleScroll(-containerRef.current!.clientWidth);
+  const scrollRight = () => handleScroll(containerRef.current!.clientWidth);
 
   return (
     <div className="flex flex-col">
       <h2 className="mb-4 text-2xl font-bold">{title}</h2>
-      <div className="flex overflow-x-auto" {...handleSwipe} ref={containerRef}>
-        {episodes.map((episode) => (
-          <div className="flex-none mr-4" key={episode.name}>
-            <EpisodeComponent episode={episode} />
-          </div>
-        ))}
+      <div className="flex items-center">
+        <button
+          className="px-2 py-1 mr-2 bg-gray-200 rounded-md hover:bg-gray-300"
+          disabled={scrollPosition === 0}
+          onClick={scrollLeft}
+        >
+          &lt;
+        </button>
+        <div
+          className="flex flex-grow overflow-x-auto"
+          ref={containerRef}
+          onScroll={() => setScrollPosition(containerRef.current?.scrollLeft ?? 0)}
+        >
+          {episodes.map((episode) => (
+            <div className="flex-none mr-4" key={episode.name}>
+              <EpisodeComponent episode={episode} />
+            </div>
+          ))}
+        </div>
+        <button
+          className="px-2 py-1 ml-2 bg-gray-200 rounded-md hover:bg-gray-300"
+          disabled={
+            (containerRef.current &&
+              containerRef.current.clientWidth + scrollPosition >= containerRef.current.scrollWidth) ??
+            undefined
+          }
+          onClick={scrollRight}
+        >
+          &gt;
+        </button>
       </div>
     </div>
   );
