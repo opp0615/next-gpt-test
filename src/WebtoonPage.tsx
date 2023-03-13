ff94f09ed5e4a35b228da542a83de418f862facb
opp0615 <opp0615@naver.com>
2023-03-13 18:39:42 +0900

    [gpt] 좋아요 상태 추가

--- src/WebtoonPage.tsx
+++ src/WebtoonPage.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Episode, Chapter } from '../src/types';
 import Webtoon from '../src/Webtoon';
 import ChaptersList from '../src/ChapterList';
 import { dummyChapters } from '../src/constant';
 
 const WebtoonPage = ({ episode }: { episode: Episode }) => {
+  const [likes, setLikes] = useState(0); // Initialize the state of likes to 0
+
+  const handleLike = () => {
+    setLikes(likes + 1); // Update the state of likes by incrementing it by 1
+  };
+
   return (
     <div className="flex flex-col md:flex-row ">
       {/* Webtoon component on top (if screen is smaller than md) */}
       <div className="flex justify-center w-full md:w-1/3  md:hidden">
-        <Webtoon episode={episode} likes={0} onLike={() => {}} />
+        <Webtoon episode={episode} likes={likes} onLike={handleLike} />
       </div>
 
       {/* ChaptersList component on bottom (if screen is smaller than md) */}
@@ -19,7 +25,7 @@ const WebtoonPage = ({ episode }: { episode: Episode }) => {
 
       {/* Webtoon component on left (if screen is larger than md) */}
       <div className="md:flex md:justify-center hidden w-1/3">
-        <Webtoon episode={episode} likes={0} onLike={() => {}} />
+        <Webtoon episode={episode} likes={likes} onLike={handleLike} />
       </div>
 
       {/* ChaptersList component on right (if screen is larger than md) */}
