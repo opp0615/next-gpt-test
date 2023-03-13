624aea5a4e34e26d38978e14b10b233bbba7013d
opp0615 <opp0615@naver.com>
2023-03-13 20:28:04 +0900

    movie 탭 최대 너비 디자인 수정

--- pages/movies.tsx
+++ pages/movies.tsx
@@ -3,7 +3,7 @@ import { useMovieQuery } from '../src/useMoiveQuery';
 
 const Movies = () => {
   const { data, isLoading, isError, error } = useMovieQuery();
-  console.log(isLoading, error, data);
+
   if (isLoading) {
     return <div>Loading...</div>;
   }
@@ -16,25 +16,27 @@ const Movies = () => {
     return <div>No data available</div>;
   }
   return (
-    <div className="flex flex-col items-center">
-      <img
-        src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
-        alt={`${data.title} poster`}
-        className="w-1/2 rounded-lg shadow-lg"
-      />
-      <h1 className="mt-6 text-3xl font-bold">{data.title}</h1>
-      <div className="flex mt-2">
-        {data.genres.map((genre) => (
-          <div key={genre.id} className="px-3 py-1 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full">
-            {genre.name}
-          </div>
-        ))}
-      </div>
-      <div className="mt-4 text-lg leading-relaxed">{data.overview}</div>
-      <div className="mt-4 text-sm font-medium">
-        <span className="mr-4">Release Date: {new Date(data.release_date).toLocaleDateString()}</span>
-        <span className="mr-4">Runtime: {data.runtime} minutes</span>
-        <span className="mr-4">IMDb Rating: {data.vote_average}</span>
+    <div className="flex justify-center">
+      <div className="flex flex-col items-center max-w-[720px]">
+        <img
+          src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
+          alt={`${data.title} poster`}
+          className="w-1/2 rounded-lg shadow-lg max-w-[512px]"
+        />
+        <h1 className="mt-6 text-3xl font-bold">{data.title}</h1>
+        <div className="flex mt-2">
+          {data.genres.map((genre) => (
+            <div key={genre.id} className="px-3 py-1 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full">
+              {genre.name}
+            </div>
+          ))}
+        </div>
+        <div className="mt-4 text-lg leading-relaxed">{data.overview}</div>
+        <div className="mt-4 text-sm font-medium">
+          <span className="mr-4">Release Date: {new Date(data.release_date).toLocaleDateString()}</span>
+          <span className="mr-4">Runtime: {data.runtime} minutes</span>
+          <span className="mr-4">IMDb Rating: {data.vote_average}</span>
+        </div>
       </div>
     </div>
   );
